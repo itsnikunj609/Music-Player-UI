e653e11091bff3210f659dd17c6acc922a6862b7
itsnikunj609 <kumarraynikunj@gmail.com>
2026-03-28 15:56:42 +0530

    .

--- script.js
+++ script.js
@@ -213,19 +213,26 @@ async function main() {
         console.log("setting volume to", e.target.value, "/100")
         currentsong.volume = parseInt(e.target.value) / 100
     })
-     // add an event listener to mute the track
+    // add an event listener to mute the track
     document.querySelector(".volume>img").addEventListener("click",e=>{
+        console.log(e.target)
         if(e.target.src.includes("volume.svg")){
-            e.target.src=e.target.src.replace("volume.svg.","mute.svg")
+            e.target.src=e.target.src.replace("volume.svg","mute.svg")
             currentsong.volume=0;
+            document.querySelector(".range").getElementsByTagName("input")[0].value=0;
         }
         else{
             currentsong.volume=0.1;
               e.target.src=e.target.src.replace("mute.svg","volume.svg")
+                          document.querySelector(".range").getElementsByTagName("input")[0].value=10;
+
         }
     })
+        
+    
   
 
 }
+
 
 main()
